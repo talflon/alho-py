5fd465cd5a4551501fdd90cbe976987e2e213c0c
Daniel Getz <tanketz@gmail.com>
2015-05-28 23:11:37 -0400

    refactor tests: add db directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,6 @@ import time
 import pytest
 
 
-@pytest.fixture
-def db():
-    from alho.db import Database, create_tables
-    import sqlite3
-    conn = sqlite3.connect(':memory:')
-    create_tables(conn)
-    return Database(conn, 12345)
-
-
 class FakeTime:
 
     def __init__(self, start, inc=0):
